e18148c96983e81d13f22b7a8f53a0594ca15a03
sugiuta <sugi.uta_1203@icloud.com>
2021-05-18 11:25:04 +0900

    edit

--- Tweak/Tweak/Tweak.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <UIKit/UIControl.h>
-#import <Cephei/HBPreferences.h>
-
-#define W [UIScreen mainScreen].bounds.size.width
-#define H [UIScreen mainScreen].bounds.size.height
